a85afce4e5207a0e8c48cfa55d8439cc0da7419c
Kelpotronic <kelpchavan1@gmail.com>
2025-06-12 15:37:38 -0400

    updated site build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/',
+  base: '/your-repo-name/', // ← make sure this is correct for GitHub Pages
   plugins: [vue()],
-});
+  build: {
+    outDir: 'docs' // for GitHub Pages
+  }
+})
+
